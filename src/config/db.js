95db90ce374989aeb652dc2a2e57fe0cb171d0f9
codinghack0810 <codinghack30@gmail.com>
2024-08-09 18:40:05 +1000

    Change Database
    Co-Authored-By: andrii-kolodii <148019621+devstar1014@users.noreply.github.com>
    Co-Authored-By: fulldev0023 <177883819+fulldev0023@users.noreply.github.com>
    Co-Authored-By: Lucas <9166113+superstar-lucas@users.noreply.github.com>

--- src/config/db.js
+++ src/config/db.js
@@ -1,25 +1,24 @@
-const mysql = require("mysql2")
+const mysql = require("mysql2");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 async function dbConnect() {
-
-  const db = mysql.createConnection({
+  const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-  })
+    database: process.env.DB_DATABASE, // Use the name of the database you created
+  });
 
-  db.connect((err) => {
+  // Connect to the MySQL database
+  connection.connect((err) => {
     if (err) {
-      console.error('Database connection failed: ' + err.stack)
-      return
-    } else {  
-      console.log('Connected to database.');      
+      console.error("Error connecting to the database: " + err.stack);
+      return;
     }
-  })
+    console.log("Connected to the database as ID " + connection.threadId);
+  });
 }
 
 module.exports = dbConnect;
