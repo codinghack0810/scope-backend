4cd8918e2b08c8f4869285e623a97207cb9f703e
codinghack0810 <codinghack30@gmail.com>
2024-08-17 23:40:35 +1000

    change user.model, requireAuth, user.controller
    Co-Authored-By: codinghack0810 <178182017+codinghack0810@users.noreply.github.com>
    Co-Authored-By: mm000810 <178183308+mm000810@users.noreply.github.com>
    Co-Authored-By: golden0810 <178187133+golden0810@users.noreply.github.com>

--- src/middlewares/requiredAuth.js
+++ src/middlewares/requiredAuth.js
@@ -14,7 +14,6 @@ module.exports = async (req, res, next) => {
                 .json({ msg: "No token, authentication denied." });
         }
         const token = req.headers.authorization;
-        console.log("token ===========> ", token);
         if (!token) {
             return res
                 .status(401)
@@ -24,26 +23,12 @@ module.exports = async (req, res, next) => {
             if (error) {
                 return res.status(401).json({ msg: "Token is not valid." });
             }
-            console.log("decoded =======> ", decoded);
 
             const userAccount = await UserAccount.findOne({
                 where: { id: decoded.id },
             });
             req.user = userAccount;
             next();
-
-            // UserAccount.findOne({ where: { id: decoded.id } })
-            //     .then((userAccount) => {
-            //         req.user = userAccount;
-            //         console.log(user);
-
-            //         next();
-            //     })
-            //     .catch(() => {
-            //         return res
-            //             .status(401)
-            //             .json({ msg: "User token is not valid." });
-            //     });
         });
     } catch (error) {
         console.error("Something wrong with auth middleware.", error);
