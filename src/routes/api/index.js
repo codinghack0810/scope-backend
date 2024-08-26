a0ced6b56f8e222d6c037ff1550ab60c5889037f
codinghack0810 <codinghack30@gmail.com>
2024-08-26 15:43:33 +0000

    change server

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./user.routes");
-const serviceRoutes = require("./service.routes");
 const transactionRoutes = require("./transaction.routes");
 
 router.use("/user", userRoutes);
-router.use("/service", serviceRoutes);
 router.use("/transaction", transactionRoutes);
 
 module.exports = router;
