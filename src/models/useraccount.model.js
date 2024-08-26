723bfdba23b8ed4dea560bdada25180c13b28d60
codinghack0810 <codinghack30@gmail.com>
2024-08-26 16:29:54 +0000

    add comment in code

--- src/models/useraccount.model.js
+++ src/models/useraccount.model.js
@@ -41,21 +41,24 @@ module.exports = (sequelize, Sequelize) => {
         },
     });
 
+    // Define a beforeCreate hook on the UserAccount model
     UserAccount.beforeCreate(async (userAccount, options) => {
+        // Check if password is provided and hash it using bcrypt
         if (userAccount.password) {
-            const salt = await bcrypt.genSalt(10);
+            const salt = await bcrypt.genSalt(10); // Generate salt with 10 rounds
             userAccount.password = await bcrypt.hash(
                 userAccount.password,
                 salt
-            );
+            ); // Hash the password with the generated salt
         }
 
+        // Check if securityAnswer is provided and hash it using bcrypt
         if (userAccount.securityAnswer) {
-            const salt = await bcrypt.genSalt(10);
+            const salt = await bcrypt.genSalt(10); // Generate salt with 10 rounds
             userAccount.securityAnswer = await bcrypt.hash(
                 userAccount.securityAnswer,
                 salt
-            );
+            ); // Hash the security answer with the generated salt
         }
     });
 
